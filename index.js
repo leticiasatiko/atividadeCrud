3376d5e768a29c3e491f74d1f2eacd8c9a33efb4
Leticia Takahashi <leticiasatikotakahashi@gmail.com>
2025-04-10 23:34:27 -0300

    estrutura inicial

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import persoRoutes from './routes/personagens.js';
 
 const app = express();
 const PORT = 5000
 
 app.use(bodyParser.json());
 
-app.use('/personagens', persoRoutes);
-
-app.get('/', (req, res) => {
-    console.log('[GET ROUTE]');
-    res.send('HELLO FROM HOMEPAGE');
-})
-
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
